cfd5f717f71742d1766834baba7f3231866ebd68
Mirai-bebya88 <malakmadze.elene@gmail.com>
2025-12-16 15:34:45 +0400

    Initial commit

---
+++ ChuckNorrisCategories/FunFactViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FunFactViewController.swift
+//  ChuckNorrisCategories
+//
+//  Created by elene malakmadze on 16.12.25.
+//
+
+import UIKit
+
+class FunFactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
